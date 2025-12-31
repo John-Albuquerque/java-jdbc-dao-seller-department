750f7602920e19f9e7bae5c02c0919381a18b9cf
Jonathan Albuquerque <jonathanalbuquerquq@gmail.com>
2025-12-31 19:50:13 -0300

    DepartmentDaoJDBC and DaoFactory

---
+++ src/models/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,44 @@
+package models.dao.impl;
+
+
+import models.dao.DepartmentDao;
+import models.entities.Department;
+
+import java.sql.Connection;
+
+import java.util.List;
+
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public void insert(Department obj) {
+
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deletedById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return null;
+    }
+}
